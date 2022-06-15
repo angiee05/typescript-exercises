c2a5b714050a911d19998d42ac82e64d25821079
Angela Balsamo <cuoreangela@hotmail.it>
2022-06-15 19:30:42 +0200

    exercise 15

--- exercise-1-complete-the-class-start.ts
+++ exercise-1-complete-the-class-start.ts
@@ -34,9 +34,9 @@ class Currency {
         // Replace the `null` values below with the appropriate field values.
         // Hint: Access field values with: this.fieldName
 
-        let description = `The ${null} currency `;
-        description += `has the code ${null} `;
-        description += `and uses the symbol ${null}.`;
+        let description = `The ${this.name} currency `;
+        description += `has the code ${this.code} `;
+        description += `and uses the symbol ${this.symbol}.`;
 
         console.log(description);
     }
